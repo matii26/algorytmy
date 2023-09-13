b2546d56340271086e2f23649f37886f95050bd8
matii26 <mat206szewa@gmail.com>
2023-09-13 14:54:38 +0200

    wyszukiwanie binarne

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,34 @@ public class Main {
         System.out.println(fibo);
         return fibo;
     }
+    public static Integer wyszukiwanieBinarne(ArrayList<Integer> ListaDoWyszukiwaniaBina,
+                                              int szukana){
+        int liczba =0;
+        int poczatek = 0;
+        int koniec = ListaDoWyszukiwaniaBina.size()-1;
+        while (koniec - poczatek>1){
+        int srodek = (poczatek+koniec)/2;
+        if (ListaDoWyszukiwaniaBina.get(srodek)>szukana){
+            koniec = srodek;
+        }
+        else {
+            poczatek = srodek;
+        }
+        }
+        int roznica1 = szukana-ListaDoWyszukiwaniaBina.get(poczatek);
+        int roznica2 = ListaDoWyszukiwaniaBina.get(koniec) - szukana;
+        if (roznica1>roznica2){
+            liczba = ListaDoWyszukiwaniaBina.get(koniec);
+        }
+        else{
+            liczba = ListaDoWyszukiwaniaBina.get(koniec);
+        }
+        return liczba;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        fibonacci();
+        ArrayList<Integer>fibki = fibonacci();
+        System.out.println(wyszukiwanieBinarne(fibki, 54));
     }
 }
