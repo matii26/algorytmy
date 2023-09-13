e0fb6aacebc9b025e7ccc28b30b2876fb0fe7658
matii26 <mat206szewa@gmail.com>
2023-09-13 14:27:34 +0200

    liczby fibonacci

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static ArrayList<Integer> fibonacci() {
+        ArrayList<Integer> fibo = new ArrayList<>();
+        fibo.add(0);
+        fibo.add(1);
+        int index =2;
+        while(fibo.size()<40){
+            fibo.add(fibo.get(index-1)+ fibo.get(index-2));
+            index++;
+        }
+        System.out.println(fibo);
+        return fibo;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        fibonacci();
     }
 }
